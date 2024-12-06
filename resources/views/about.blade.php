e7c1d8f51a316389e573ead907ff4085352aa569
Douglas White <douglas.rw@outlook.com>
2024-12-05 18:20:34 -0700

    Created the layout file with navlinks. Imported a new navlink UI components from TailwindUI. Made all of the links functioing to the three routes present

---
+++ resources/views/about.blade.php
@@ -0,0 +1,8 @@
+<x-layout>
+
+    <x-slot:heading>
+        About Page
+    </x-slot:heading>
+    <h1>Hello from about page</h1>
+
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,18 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
